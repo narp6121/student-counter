dadf19c6de1ace42e68ed4415a745a9fa748361b
narp6121 <narp6121@gmail.com>
2025-07-01 03:10:07 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v9'; // 다음 업데이트 때 'v3', 'v4'로 바꾸기
+const CACHE_VERSION = 'v10'; // 다음 업데이트 때 'v3', 'v4'로 바꾸기
 const CACHE_NAME = `my-cache-${CACHE_VERSION}`;
 const FILES_TO_CACHE = [
   './',
