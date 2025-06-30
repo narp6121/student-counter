58073db50f5229af634cc72ef03d02837ba9af33
narp6121 <narp6121@gmail.com>
2025-07-01 00:30:29 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,42 @@
+const CACHE_VERSION = 'v2'; // 다음 업데이트 때 'v3', 'v4'로 바꾸기
+const CACHE_NAME = `my-cache-${CACHE_VERSION}`;
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// 설치 이벤트
 self.addEventListener('install', function(e) {
   console.log('[SW] Installed');
-  self.skipWaiting(); // 새로운 서비스워커 바로 활성화
+  self.skipWaiting(); // 새 서비스워커 바로 활성화
   e.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       console.log('[SW] Caching files...');
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+// 활성화 이벤트 - 이전 캐시 삭제
 self.addEventListener('activate', function(e) {
   console.log('[SW] Activated');
   e.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          console.log('[SW] Deleting old cache:', cacheName);
-          return caches.delete(cacheName);
+        cacheNames.map(function(cache) {
+          if (cache !== CACHE_NAME) {
+            console.log('[SW] Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
         })
       );
     })
   );
 });
 
+// fetch 이벤트
 self.addEventListener('fetch', function(e) {
   console.log('[SW] Fetching:', e.request.url);
   e.respondWith(
