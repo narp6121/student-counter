029dec9eaa2b34aa81d894ef6424fe241c5b6329
narp6121 <narp6121@gmail.com>
2025-07-01 00:20:27 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
-
 self.addEventListener('install', function(e) {
+  console.log('[SW] Installed');
+  self.skipWaiting(); // 새로운 서비스워커 바로 활성화
   e.waitUntil(
     caches.open('v1').then(function(cache) {
+      console.log('[SW] Caching files...');
       return cache.addAll([
         './',
         './index.html',
@@ -11,7 +13,23 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+self.addEventListener('activate', function(e) {
+  console.log('[SW] Activated');
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          console.log('[SW] Deleting old cache:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(e) {
+  console.log('[SW] Fetching:', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
